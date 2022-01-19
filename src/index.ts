627c88e73df906f901849906bb419a97c8e5e993
Augusto Fontana <adfontana@gmail.com>
2022-01-19 12:00:58 -0300

    Setting class-tranformer

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import users from './data/users.json';
 import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { User } from './modules/user/User';
 
-const instanceObjs = plainToInstance(User, users);
-
-const plainObjs = instanceToPlain(instanceObjs);
-
+console.log('Using instanced objects');
+const instanceObjs = plainToInstance(User, users, { excludeExtraneousValues: true });
 console.log(instanceObjs.map(item => item.getName()));
+
+console.log('');
+
+console.log('Instanced objects to plain objects');
+const plainObjs = instanceToPlain(instanceObjs, { exposeUnsetFields: false });
 console.log(plainObjs);
--- src/modules/user/User.ts
+++ src/modules/user/User.ts
@@ -1,22 +1,46 @@
-export class User {
-    
-    id: number;
-    firstName: string;
-    lastName: string;
-    age: number;
+import { Expose, Type } from "class-transformer";
 
-    constructor() {
-        this.id = 0;
-        this.firstName = '';
-        this.lastName = '';
-        this.age = 0;
-    }
-  
-    getName() {
-      return this.firstName + ' ' + this.lastName;
-    }
-  
-    isAdult() {
-      return this.age > 36 && this.age < 60;
-    }
+class Category {
+  @Expose() id: string;
+  @Expose() name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
   }
+}
+
+export class User {
+
+  @Expose({ name: 'category' })
+  @Type(() => Category)
+  private _category?: Category;
+
+  @Expose() id: number;
+  @Expose() firstName: string;
+  @Expose() lastName: string;
+  @Expose() age: number;
+
+
+  constructor() {
+    this.id = 0;
+    this.firstName = '';
+    this.lastName = '';
+    this.age = 0;
+  }
+
+  get category(): Category {
+    if (!this._category) {
+      this._category = new Category('1', 'category');
+    }
+    return this._category;
+  }
+
+  getName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  isAdult() {
+    return this.age > 36 && this.age < 60;
+  }
+}
